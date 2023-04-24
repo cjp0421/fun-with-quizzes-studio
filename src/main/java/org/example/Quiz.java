eed8076059ac3522696f41490a4331cea386c13b
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2023-04-23 21:09:08 -0500

    completed the studio

--- src/main/java/org/example/Quiz.java
+++ src/main/java/org/example/Quiz.java
@@ -43,8 +43,42 @@ public class Quiz {
           ArrayList<Integer> correctResponses = new ArrayList<>();
           //loops through the numbers associated with the answers
           for (int choiceNum : question.getChoiceMap().keySet()){
-              if(question.getChoiceMap().get(choiceNum).isCorrect()){}
+              //creates an array of choiceNums that indicate the correct answer(s
+              if(question.getChoiceMap().get(choiceNum).isCorrect()){
+                  correctResponses.add(choiceNum);
+              }
           }
+
+          //get the actual responses and evaluate them against the correct choiceNums (above)
+            ArrayList<Integer> userResponses = new ArrayList<>();
+          //using a while loop and an outside incrementor allows the loop to account for the possibility the user
+            // will try to enter the exact same correct answer more than once - to fool the grading loop
+          //the incrementor (i) allows us to keep track of how many times the user enters an answer
+            int i = 0;
+            while(i < correctResponses.size()){
+                System.out.println("Please enter a number: ");
+                //typically, validation of user input should be included here to make sure
+                //the type of the user input won't break the program
+                //TO DO for later: user input validation
+                int userResponse = input.nextInt();
+                if(userResponses.contains(userResponse)){
+                    System.out.println("You already gave that answer. Please try again.");
+                } else if(!correctResponses.contains(userResponse)){
+                    System.out.println("Sorry, that is incorrect. The correct answer is: ");
+                    for(int resp : correctResponses){
+                        System.out.println(question.getChoiceMap().get(resp).getContent());
+                    }
+                    break;
+                } else {
+                    System.out.println("Correct!");
+                    userResponses.add(userResponse);
+                    if(i == correctResponses.size() - 1){
+                        updateScore();
+                    }
+                    i++;
+                }
+            }
+
 
         }
 
--- src/main/java/org/example/QuizRunner.java
+++ src/main/java/org/example/QuizRunner.java
@@ -3,7 +3,42 @@ package org.example;
 import java.util.ArrayList;
 
 public class QuizRunner {
-    public static void main(String[] args) {
+//if you create a field outside of the main method
+    //you have to designate it as static
+    private static final Quiz quiz = new Quiz();
 
+    public static void main(String[] args) {
+        String q1 = "Which of the following are books by Jane Austen?";
+        Choice[] q1Choices = new Choice[] {
+                new Choice("Jane Eyre"),
+                new Choice("Pride and Prejudice", true),
+                new Choice("Wives and Daughters"),
+                new Choice("Mansfield Park", true)
+        };
+        CheckBoxQuestion question1 = new CheckBoxQuestion(q1, q1Choices);
+
+        String q2 = "The most-published author of all time is Agatha Christie.";
+        Choice[] q2Choices = new Choice[] {
+          new Choice("True", true),
+          new Choice("False")
+        };
+        TrueFalseQuestion question2 = new TrueFalseQuestion(q2, q2Choices);
+
+        String q3 = "Which poem did Amanda Gorman recite at the U.S. presidential inauguration in 2021?";
+        Choice[] q3Choices = new Choice[]{
+                new Choice("The Hill We Climb", true),
+                new Choice("The Miracle of Morning"),
+                new Choice("We Rise"),
+                new Choice("New Day's Lyric")
+        };
+        MultipleChoiceQuestion question3 = new MultipleChoiceQuestion(q3, q3Choices);
+
+        quiz.addQuestions(new Question[] {question1,question2, question3});
+
+        quiz.runQuiz();
+
+        System.out.println("\nYou answered " + quiz.getNumCorrect()+
+                " of " + quiz.getQuestions().size() +
+                " questions correctly. Thanks for playing!");
     }
 }
